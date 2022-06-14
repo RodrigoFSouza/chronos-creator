396c4c234cbd911b8090dce721e22a6d77ea7984
Rodrigo Souza <rodrigo.souza@lumis.com.br>
2022-06-13 23:50:32 -0300

    iniciando o projeto

---
+++ src/main/java/com/chronos/chronoscreator/ChronosCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.chronos.chronoscreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChronosCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chronos/chronoscreator/ChronosCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronos.chronoscreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronosCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
